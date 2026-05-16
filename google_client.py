a848a21fd393390979781581b12dfd76caefeb15
ahramcov <ahramcov2018@yandex.ru>
2026-05-16 17:23:08 +0300

    chore: log google credential metadata

--- google_client.py
+++ google_client.py
@@ -1,4 +1,5 @@
 import gspread
+import json
 
 from config import GOOGLE_AUTH_PATH
 
@@ -9,4 +10,13 @@ def get_gspread_client():
             f"Google credentials file not found: {GOOGLE_AUTH_PATH}. "
             "Container entrypoint should create it from GOOGLE_AUTH_JSON or GOOGLE_AUTH_JSON_BASE64."
         )
+    data = json.loads(GOOGLE_AUTH_PATH.read_text(encoding="utf-8"))
+    print(
+        "Using Google credentials file",
+        str(GOOGLE_AUTH_PATH),
+        "client_email=",
+        data.get("client_email"),
+        "private_key_id=",
+        data.get("private_key_id"),
+    )
     return gspread.service_account(filename=str(GOOGLE_AUTH_PATH))
